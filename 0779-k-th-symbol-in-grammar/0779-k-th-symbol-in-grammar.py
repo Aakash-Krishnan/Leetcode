92cd0254ab7496dace7242653364f9a2409c46a7
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2023-09-14 14:04:33 +0530

    Time: 44 ms (28.21%), Space: 16.3 MB (70.52%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -1,11 +1,12 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        if n == 1: return 0
+        if n == 1:
+            return 0
         
         parent = self.kthGrammar(n-1, k//2 + k%2)
-        isOdd = k%2 == 1
+        isOdd = k % 2 == 1
         
-        if parent == 1:
-            return 1 if isOdd else 0
-        else:
+        if parent == 0:
             return 0 if isOdd else 1
+        else:
+            return 1 if isOdd else 0
