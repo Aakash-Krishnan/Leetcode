fb34a988a944be1a50c43a8995768e36b0237e30
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2023-11-22 21:38:34 +0530

    Time: 364 ms (5.24%), Space: 24.3 MB (67.83%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,26 +1,18 @@
 class Solution:
     def findDuplicates(self, nums):
-#         i = 0
-#         ans = []
-#         while i < len(nums):
-#             idx = nums[i]-1
-#             if nums[i] == -1 or nums[i] == i+1:
-#                 i += 1
+        i = 0
+        ans = []
+        while i < len(nums):
+            idx = nums[i]-1
+            if nums[i] == -1 or nums[i] == i+1:
+                i += 1
 
-#             elif nums[i] == nums[idx]:
-#                 ans.append(nums[i])
-#                 nums[i] = -1
-#                 nums[idx] = -1
+            elif nums[i] == nums[idx]:
+                ans.append(nums[i])
+                nums[i] = -1
+                nums[idx] = -1
                 
-#             elif nums[i] != nums[idx]:
-#                 nums[i], nums[idx] = nums[idx], nums[i]
+            elif nums[i] != nums[idx]:
+                nums[i], nums[idx] = nums[idx], nums[i]
         
-#         return ans
-
-            ans = []
-            for i in range(len(nums)):
-                val = abs(nums[i]) 
-                if nums[val - 1] < 0:
-                    ans.append(val)
-                nums[val - 1] = -nums[val - 1]
-            return ans
+        return ans
