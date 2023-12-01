906c5765a7b3c5a61bd14736d2d3719e25ff39fc
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2023-12-01 09:06:27 +0530

    Time: 39 ms (66.51%), Space: 16.5 MB (28.02%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -2,11 +2,10 @@ class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         Sum1 = ""
         Sum2 = ""
-        for i in word1:
-            Sum1+= i
-        for i in word2:
-            Sum2+= i
-        sorted(Sum1)
-        sorted(Sum2)
+        n,m = len(word1), len(word2)
+        for i in range(max(n, m)):
+            if i < n:
+                Sum1 += word1[i]
+            if i < m:
+                Sum2+= word2[i]
         return Sum1 == Sum2
-        
