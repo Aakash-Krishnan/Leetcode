47ec1ee3b143c62e3ccdc95dac03bc80f197cd1c
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2023-12-01 15:41:39 +0530

    Time: 42 ms (33.24%), Space: 16.3 MB (41.83%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,12 +1,11 @@
 class Solution:
     def checkIfPangram(self, A):
-        A = sorted(A)
-        val = 97
-        for i in A:
-            # if ord(i) > val:
-            #     return False
-            if ord(i) == val:
-                val += 1
-        if val == 123:
-            return True
-        return False
+        # A = sorted(A)
+        # val = 97
+        # for i in A:
+        #     if ord(i) == val:
+        #         val += 1
+        # if val == 123:
+        #     return True
+        # return False
+        return len(set(A)) == 26
