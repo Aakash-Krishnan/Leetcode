a06c7fc6fbc8740581ea96e599bc0dc8ddc8b336
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2023-12-05 21:23:18 +0530

    Time: 38 ms (50.35%), Space: 16.2 MB (71.13%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        if n == 0:
+            return 0
+        
+        if n != 1 and (n & 1):
+            val = self.numberOfMatches( ((n-1)//2) + 1)
+            return ((n-1)//2) + val
+        else:
+            val = self.numberOfMatches( n // 2 )
+            if (n//2) == 1:
+                return (n//2)
+            return (n//2) + val
