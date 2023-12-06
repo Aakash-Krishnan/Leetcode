9d4cec8f4a1a3a80f44ae4bb590673a67f714ce2
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2023-12-06 17:23:15 +0530

    Time: 51 ms (5.70%), Space: 16.2 MB (73.58%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        ans = 0
+        m = 0
+        start = 1
+
+        while m <= n:
+            print(start, m, ans)
+            if (m + 7) <= n:
+                ans += ((7)*(start + (start + 6))) // 2
+            else:
+                end = n - m
+                ans += ((end)*(start + (start + end-1))) // 2
+
+            start += 1
+            m += 7 
+        return ans
