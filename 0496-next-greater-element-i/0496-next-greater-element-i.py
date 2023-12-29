f0cdd76f33c7d0f1e9cb0b6628e0fb5009b97160
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2023-12-29 19:21:53 +0530

    Time: 114 ms (13.70%), Space: 17.6 MB (8.34%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -5,15 +5,15 @@ class Solution:
         ans = []
         
         for i in range(len(nums2)-1, -1, -1):
-            while stack and nums2[stack[-1]] <= nums2[i]:
+            while stack and stack[-1] <= nums2[i]:
                 stack.pop()
             
             if stack:
-                cache[nums2[i]] = nums2[stack[-1]]
+                cache[nums2[i]] = stack[-1]
             else:
                 cache[nums2[i]] = -1
             
-            stack.append(i)
+            stack.append(nums2[i])
             
         for i in nums1:
             ans.append(cache[i])
