49bccef2b0b6d820846120d5f000e8b94f251517
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2023-12-29 19:16:24 +0530

    Time: 71 ms (27.68%), Space: 17.7 MB (8.34%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,6 +2,7 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         cache = {}
         stack = []
+        ans = []
         
         for i in range(len(nums2)-1, -1, -1):
             while stack and nums2[stack[-1]] <= nums2[i]:
@@ -14,8 +15,7 @@ class Solution:
             
             stack.append(i)
             
-        stack.clear()
         for i in nums1:
-            stack.append(cache[i])
+            ans.append(cache[i])
         
-        return stack
+        return ans
