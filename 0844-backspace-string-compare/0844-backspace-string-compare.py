b8b20d182b7fc6be70673afeb9992d93827a6f84
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2023-12-31 19:28:26 +0530

    Time: 47 ms (9.10%), Space: 17.2 MB (10.85%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,20 +1,33 @@
 class Solution:
-    def backspaceCompare(self, s: str, t: str) -> bool:
-        Sstack = []
-        Tstack = []
+    def backspaceCompare(self, s: str, t: str) -> bool:     
+    
+        def helper(string, index):
+            
+            backspace = 0
+            while index >= 0:
+                if backspace == 0 and string[index] != "#":
+                    break
+                elif string[index] == "#":
+                    backspace += 1
+                else:
+                    backspace -= 1
+                index -= 1
+                
+            return index
         
-        for i in s:
-            if i == "#":
-                if Sstack:
-                    Sstack.pop()
-            else:
-                Sstack.append(i)
+        index_s, index_t = len(s)-1, len(t)-1
         
-        for i in t:
-            if i == "#":
-                if Tstack:
-                    Tstack.pop()
-            else:
-                Tstack.append(i)
-        
-        return True if Sstack == Tstack else False
+        while index_s >= 0 or index_t >= 0:
+            index_s = helper(s, index_s)
+            index_t = helper(t, index_t)
+            
+            char_s = s[index_s] if index_s >= 0 else ""
+            char_t = t[index_t] if index_t >= 0 else ""
+            
+            if char_s != char_t:
+                return False
+            
+            index_s -= 1
+            index_t -= 1
+            
+        return True
