f1c9246eca5206718edcf381b4e7683967fa97af
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2024-01-01 16:07:14 +0530

    Time: 74 ms (48.59%), Space: 17.9 MB (10.08%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -7,18 +7,15 @@ class Solution:
             return val1, val2
         
         for i in tokens:
+            val1, val2 = popOut() if i in ["+", "-", "*", "/"] else (0, 0)
             match i:
                 case "+":
-                    val1, val2 = popOut()
                     stack.append(val1 + val2)
                 case "-":
-                    val1, val2 = popOut()
                     stack.append(val1 - val2)
                 case "*":
-                    val1, val2 = popOut()
                     stack.append(val1 * val2)
                 case "/":
-                    val1, val2 = popOut()
                     stack.append(int(val1 / val2))
                 case default:
                     stack.append(int(i))
