56633aed4f3c86475807883d90407ef3f40965f3
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2024-01-19 10:23:44 +0530

    Time: 122 ms (77.46%), Space: 18.3 MB (30.83%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,21 +1,35 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         N = len(matrix)
-        cache = {}
         
-        def path(r, c):
-            if r == N:
-                return 0
-            if c < 0 or c == N:
-                return float("inf")
-            if (r, c) in cache:
-                return cache[(r, c)]
+        for i in range(1, N):
+            for j in range(N):
+                left = matrix[i-1][j-1] if j-1 >= 0 else float("inf")
+                up = matrix[i-1][j]
+                right = matrix[i-1][j+1] if j+1 < N else float("inf")
+                
+                matrix[i][j] += min(left, up, right)
+        return min(matrix[-1])
+                
+        
+        
+        
+#         N = len(matrix)
+#         cache = {}
+        
+#         def path(r, c):
+#             if r == N:
+#                 return 0
+#             if c < 0 or c == N:
+#                 return float("inf")
+#             if (r, c) in cache:
+#                 return cache[(r, c)]
             
-            cache[(r,c)] = matrix[r][c] + min(path(r+1, c-1), path(r+1, c), path(r+1, c+1))
-            return cache[(r, c)]
+#             cache[(r,c)] = matrix[r][c] + min(path(r+1, c-1), path(r+1, c), path(r+1, c+1))
+#             return cache[(r, c)]
         
-        res = float("inf")
-        for i in range(len(matrix)):
-            res = min(res, path(0, i))
+#         res = float("inf")
+#         for i in range(len(matrix)):
+#             res = min(res, path(0, i))
         
-        return res
+#         return res
