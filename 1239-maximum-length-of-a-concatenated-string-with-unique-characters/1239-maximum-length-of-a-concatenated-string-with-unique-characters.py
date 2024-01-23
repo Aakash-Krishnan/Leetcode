460800da6a510abd8f9a9040584d331a39c79cf1
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2024-01-23 19:36:39 +0530

    Time: 78 ms (80.11%), Space: 16.7 MB (75.18%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -2,25 +2,24 @@ class Solution:
     def maxLength(self, arr: list[str]) -> int: 
 
         def isEqual(word1, word2):
-            wordset = set(word1)
-            for i in word2:
-                if i in wordset:
+
+            for i in word1:
+                if i in word2:
                     return True
             return False 
             
-        # res = 0
         def check(idx, word=""):
-            # nonlocal res
             if idx == len(arr):
-                # res = max(res, len(word))
                 return len(word)
             
-            if len(set(arr[idx])) != len(arr[idx]):
+            # If the string itself has duplicate chars we can skip 
+            word1 = set(arr[idx])
+            if len(word1) != len(arr[idx]):
                 return check(idx + 1, word)
+            
             res = 0
-            if not isEqual(word, arr[idx]):
+            if not isEqual(word, word1):
                 res = check(idx + 1, word + arr[idx])
 
             return max(res, check(idx+1, word))
-        # check(0)
         return check(0)
