ca5082f265b24e3a66dfee47ce4b08a1ca294267
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2024-01-28 17:57:40 +0530

    Time: 230 ms (75.35%), Space: 19.5 MB (65.42%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,13 +1,14 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        cache = {0: 1}
+        cache = defaultdict(int)
+        cache[0] = 1
         pSum = 0
         res = 0
         for i in range(len(nums)):
             pSum += nums[i]
             if pSum - k in cache:
                 res += cache[pSum - k]      
-            cache[pSum] = cache.get(pSum, 0) + 1
+            cache[pSum] += 1
         
         return res
                 
