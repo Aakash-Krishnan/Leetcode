4f4ed89bdff064aca83b2d8e9be92ca159d5d00c
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2024-02-10 10:45:29 +0530

    Time: 74 ms (86.92%), Space: 16.5 MB (86.36%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,18 +1,15 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         def palamdrome(L, R):
-            nonlocal res
-            if L < 0 or R == len(s):
-                return 
-            if s[L] == s[R]:
-                res += 1
-                palamdrome(L-1, R + 1)
-            return 
+            cnt = 0
+            while L > -1 and R < len(s) and s[L] == s[R]:
+                cnt += 1
+                L -= 1
+                R += 1
+            return cnt
         
         res = 0
         for i in range(len(s)):
-            res += 1
-            odd = palamdrome(i-1, i+1)
-            even = palamdrome(i, i+1)
-        
+            res += palamdrome(i, i)
+            res += palamdrome(i, i+1)
         return res
