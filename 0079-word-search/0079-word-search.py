b66ce894869f8b41b5d9faa64a9db22757fec6fd
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2024-02-15 20:49:06 +0530

    Time: 3246 ms (81.86%), Space: 16.3 MB (99.49%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,25 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n, m = len(board), len(board[0])
+        visited = [[0] * m for _ in range(n)]
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (r < 0 or c < 0 or r >= n or c >= m) or visited[r][c] or board[r][c] != word[i]:
+                return False
+            
+            visited[r][c] = 1
+            res = (dfs(r+1, c, i+1) or
+                  dfs(r, c+1, i+1) or
+                  dfs(r-1, c, i+1) or
+                  dfs(r, c-1, i+1))
+            
+            visited[r][c] = 0
+            return res
+        
+        for i in range(n):
+            for j in range(m):
+                if dfs(i, j, 0): return True
+        
+        return False
