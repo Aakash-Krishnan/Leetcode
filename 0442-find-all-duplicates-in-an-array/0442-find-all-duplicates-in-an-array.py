dd7e61be042190a82aa23747c54a0391abface17
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2024-02-17 14:42:59 +0530

    Time: 283 ms (34.29%), Space: 24.3 MB (88.57%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,18 +1,26 @@
 class Solution:
     def findDuplicates(self, nums):
-        i = 0
-        ans = []
-        while i < len(nums):
-            idx = nums[i]-1
-            if nums[i] == -1 or nums[i] == i+1:
-                i += 1
-
-            elif nums[i] == nums[idx]:
-                ans.append(nums[i])
-                nums[i] = -1
-                nums[idx] = -1
-                
-            elif nums[i] != nums[idx]:
-                nums[i], nums[idx] = nums[idx], nums[i]
+        res = []
+        for i in range(len(nums)):
+            x = abs(nums[i])-1
+            if nums[x] < 0:
+                res.append(x+1)
+            nums[x] = -abs(nums[x])
+        return res
         
-        return ans
+#         i = 0
+#         ans = []
+#         while i < len(nums):
+#             idx = nums[i]-1
+#             if nums[i] == -1 or nums[i] == i+1:
+#                 i += 1
+
+#             elif nums[i] == nums[idx]:
+#                 ans.append(nums[i])
+#                 nums[i] = -1
+#                 nums[idx] = -1
+                
+#             elif nums[i] != nums[idx]:
+#                 nums[i], nums[idx] = nums[idx], nums[i]
+        
+#         return ans
