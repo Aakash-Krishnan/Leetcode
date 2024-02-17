33dc2e61e8b414f1b7ca2a80d046cd7f253b0583
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2024-02-17 16:42:43 +0530

    Time: 55 ms (24.22%), Space: 16.6 MB (82.28%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,27 +1,19 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        fmap = {
-            "I": 1,
-            "IV": 4,
-            "V": 5,
-            "IX": 9,
-            "X": 10,
-            "XL": 40,
-            "L": 50,
-            "XC": 90,
-            "C": 100,
-            "CD": 400,
-            "D": 500,
-            "CM": 900,
-            "M": 1000,
+        cache = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
         }
-        total = 0
-        for i in range(len(s)-1):
-            if fmap[s[i]] < fmap[s[i+1]]:
-                total -= fmap[s[i]]
+        res = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and cache[s[i]] < cache[s[i+1]]:
+                res -= cache[s[i]]
             else:
-                total += fmap[s[i]]
-        total += fmap[s[-1]]
-        return total
-                    
-                
+                res += cache[s[i]]
+            
+        return res
