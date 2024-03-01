a8ba94e9b3fc133feca9a7426aac28c48853c697
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2024-03-01 13:14:23 +0530

    Time: 576 ms (5.54%), Space: 69.3 MB (7.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,16 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        cache = {}
+        cache = [[-1] * (sum(nums)+1) for _ in range(len(nums))]
         
         def dfs(i, Sum):
             if i >= len(nums):
                 return Sum
-            if (i, Sum) in cache:
-                return cache[(i, Sum)]
+            if cache[i][Sum] != -1:
+                return cache[i][Sum]
             
-            cache[(i, Sum)] = max(dfs(i+1, Sum) , dfs(i+2, Sum + nums[i]))
-            return cache[(i, Sum)]
+            cache[i][Sum] = max(dfs(i+1, Sum) , dfs(i+2, Sum + nums[i]))
+            return cache[i][Sum]
         
         return dfs(0, 0)
         
