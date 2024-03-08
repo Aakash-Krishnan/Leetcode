e654bd8d04cd90232be5343e85392ca50458c131
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2024-03-08 09:33:21 +0530

    Time: 51 ms (11.57%), Space: 16.5 MB (83.74%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.py
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
         count = Counter(nums)
-        maxHq = []
+        Max = max(count.values())
+        res = 0
         for key, freq in count.items():
-            heappush(maxHq, -freq)
-            
-        res = -maxHq[0]
-        prev = heappop(maxHq)
+            if freq == Max:
+                res += 1
         
-        while maxHq and maxHq[0] == prev:
-            res -= heappop(maxHq)
-        
-        return res
+        return res * Max
