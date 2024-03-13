25ce2b785c7ef2f0a8e876391e5df4c6bf83aa8e
Aakash-Krishnan <139685915+Aakash-Krishnan@users.noreply.github.com>
2024-03-13 17:48:36 +0530

    Time: 51 ms (93.11%), Space: 17.8 MB (26.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        flag = False
-        for i, value in enumerate(nums):
-            x = target - value
-            for j, innerValue in enumerate(nums):
-                if i != j:
-                    if x == innerValue:
-                        flag = True
-                        return [i , j]
-            if flag:
-                break
-
-
+        fmap = {}
+        
+        for i in range(len(nums)):
+            val = target - nums[i]
+            if val in fmap:
+                return [i, fmap[val]]
+            fmap[nums[i]] = i
+        
+        
